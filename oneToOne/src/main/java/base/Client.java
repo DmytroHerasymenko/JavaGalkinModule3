e2b6ca766020c1a23047780f8dd8ee477c466e16
Dmytro Herasymenko <mcdimamc@gmail.com>
2017-01-26 20:26:19 +0200

    OneToOne

---
+++ oneToOne/src/main/java/base/Client.java
@@ -0,0 +1,56 @@
+package base;
+
+import javax.persistence.*;
+
+/**
+ * Created by dima on 26.01.17.
+ */
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue
+    int id;
+    @Column(name = "name")
+    String name;
+    @Column(name = "surname")
+    String surname;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_address")
+    Address addres;
+
+    public Client() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Address getAddres() {
+        return addres;
+    }
+
+    public void setAddres(Address addres) {
+        this.addres = addres;
+    }
+}
