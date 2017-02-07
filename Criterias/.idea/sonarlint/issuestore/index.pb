946d7aca8d2caa79b5ec7cbca6567084cd2adaec
Dmytro Herasymenko <mcdimamc@gmail.com>
2017-02-07 21:15:27 +0200

    Criteria.hard join

---
+++ Criterias/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+M
+src/main/java/base/Human.java,6/0/60c6ee728045eaf512afe3e22b64ac384c8a2669
+L
+src/main/java/base/Bank.java,6/c/6c220daadddea35718c8813de6a536a3c8a06b4d
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+L
+src/main/java/base/Main.java,8/a/8aa96fc57fbd554b9fb64600b5faa07d98ea38b0
+=
+Criterias.iml,4/e/4e267ccabedac1d6e6a996228bf10d25eb0c57ae
--- Criterias/src/main/java/base/Main.java
+++ Criterias/src/main/java/base/Main.java
@@ -1,16 +1,14 @@
 package base;
 
-import org.hibernate.Criteria;
+import base.Bank_;
+import base.Human_;
+
 import org.hibernate.SessionFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.ParameterExpression;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 /**
@@ -37,6 +35,49 @@ public class Main {
         TypedQuery<Bank> bankTypedQuery = entityManager.createQuery(criteriaQuery).setParameter(bankName, "Kreschatyk");
         Bank b = bankTypedQuery.getSingleResult();
         System.out.println(b.getName());
+
+        entityManager.getTransaction().commit();
+
+        entityManager.getTransaction().begin();
+        CriteriaBuilder criteriaBuilder1 = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Bank> criteriaQuery1 = criteriaBuilder1.createQuery(Bank.class);
+        Root<Bank> rootBank1 = criteriaQuery1.from(Bank.class);
+        criteriaQuery1.select(rootBank1);
+
+        ParameterExpression<String> clientName = criteriaBuilder1.parameter(String.class);
+
+        Join<Bank,Human> bankHumanJoin = rootBank1.join(Bank_.humans);
+        criteriaQuery1.where(criteriaBuilder1
+                .equal(bankHumanJoin.get(Human_.name), clientName));
+
+        TypedQuery<Bank> bankTypedQuery1 = entityManager.createQuery(criteriaQuery1).setParameter(clientName,"Vasyl");
+        final List<Bank> banks = bankTypedQuery1.getResultList();
+        for (Bank bb: banks) {
+            System.out.println(bb.getName());
+        }
+        entityManager.getTransaction().commit();
+
+        entityManager.getTransaction().begin();
+        CriteriaBuilder criteriaBuilder2 = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Bank> criteriaQuery2 = criteriaBuilder2.createQuery(Bank.class);
+        Root<Bank> rootBank2 = criteriaQuery2.from(Bank.class);
+
+        Subquery<Integer> subquery = criteriaQuery2.subquery(Integer.class);
+        Root<Human> humanRoot2 = subquery.from(Human.class);
+        Join<Human, Bank> join1 = humanRoot2.join(Human_.bank);
+
+        subquery.select(join1.get(Bank_.id)).where(criteriaBuilder2
+                .equal(humanRoot2.get(Human_.name), criteriaBuilder2.parameter(String.class, "name")));
+
+        criteriaQuery2.select(rootBank2).where(criteriaBuilder2.in(rootBank2.get(Bank_.id)).value(subquery));
+        TypedQuery<Bank> bankTypedQuery2 = entityManager.createQuery(criteriaQuery2).setParameter("name","Vasyl");
+        final List<Bank> banks2 = bankTypedQuery2.getResultList();
+        for (Bank bb: banks2) {
+            System.out.println(bb.getName());
+        }
+
 
         entityManager.getTransaction().commit();
 
